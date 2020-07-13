3b3eb8db91c0230426be0d32349180413c8e4244
Tejas Vedak <52121139+tejasvedak@users.noreply.github.com>
2020-07-14 03:37:16 +0530

    Add files via upload

---
+++ build/precache-manifest.51a5a7bf9c867eb00edcc4a8d9f9bd98.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d725bc3ee9463d4b7238c74e1077dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596a15e0fb8b888ce5d0",
+    "url": "/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "30c9b5cc58fbd5aad056",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "7868ce92be1444ea8017",
+    "url": "/static/css/main.f9237b2a.chunk.css"
+  },
+  {
+    "revision": "8136489d266159c740ff",
+    "url": "/static/js/2.d359e424.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.d359e424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596a15e0fb8b888ce5d0",
+    "url": "/static/js/3.48d0119f.chunk.js"
+  },
+  {
+    "revision": "30c9b5cc58fbd5aad056",
+    "url": "/static/js/4.765f4c33.chunk.js"
+  },
+  {
+    "revision": "7868ce92be1444ea8017",
+    "url": "/static/js/main.56751669.chunk.js"
+  },
+  {
+    "revision": "e11d03594676e364324b",
+    "url": "/static/js/runtime-main.327c31d1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "7e50e67c9730510be061086cbd7423a2",
+    "url": "/static/media/coronogo.7e50e67c.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "1e402f964e4508637c68ede8da9d3cc3",
+    "url": "/static/media/pollpeeps.1e402f96.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "f5583a21a07b47a9674669575860eb4d",
+    "url": "/static/media/socially.f5583a21.webp"
+  }
+]);
